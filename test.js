5314565868eaf4e00ed9a08fc9d5471db1c69370
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-06 23:31:58 -0700

    Refactor tests with runSeries

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var Memento = require('./')
 var levelup = require('levelup')
 var memdown = require('memdown')
+var runSeries = require('run-series')
 var tape = require('tape')
 
 tape('set', function (test) {
@@ -14,15 +15,17 @@ tape('set', function (test) {
 
 tape('read', function (test) {
   var log = testInstance()
-  log.set('a', 'apple', function () {
-    log.set('a', 'atom', function () {
+  runSeries([
+    log.set.bind(log, 'a', 'apple'),
+    log.set.bind(log, 'a', 'atom'),
+    function (done) {
       log.read('a', function (error, value) {
         test.ifError(error, 'no error')
         test.equal(value, 'atom', 'yields latest value')
-        test.end()
+        done()
       })
-    })
-  })
+    }
+  ], test.end)
 })
 
 tape('read unseen key', function (test) {
@@ -36,60 +39,61 @@ tape('read unseen key', function (test) {
 
 tape('unset', function (test) {
   var log = testInstance()
-  log.set('a', 'apple', function () {
-    log.unset('a', function (error) {
-      test.ifError(error, 'no error')
+  runSeries([
+    log.set.bind(log, 'a', 'apple'),
+    log.unset.bind(log, 'a'),
+    function (done) {
       log.read('a', function (error, value) {
         test.ifError(error, 'no error')
         test.equal(value, undefined, 'yields undefined')
-        test.end()
+        done()
       })
-    })
-  })
+    }
+  ], test.end)
 })
 
 tape('head', function (test) {
   var log = testInstance()
-  log.set('a', 'apple', function () {
-    log.set('b', 'bicycle', function () {
+  runSeries([
+    log.set.bind(log, 'a', 'apple'),
+    log.set.bind(log, 'b', 'bicycle'),
+    function (done) {
       log.head(function (error, head) {
         test.ifError(error, 'no error')
         test.equal(head, 2, 'head is 2')
-        test.end()
+        done()
       })
-    })
-  })
+    }
+  ], test.end)
 })
 
 tape('rewind', function (test) {
   var log = testInstance()
-  log.set('a', 'apple', function () {
-    log.set('a', 'atom', function () {
-      log.set('b', 'bicycle', function () {
-        log.set('b', 'ball', function () {
-          log.set('c', 'cat', function () {
-            log.unset('c', function () {
-              var buffer = []
-              log.createRewindStream()
-              .on('data', function (entry) { buffer.push(entry) })
-              .once('error', function (error) { test.fail(error) })
-              .once('end', function () {
-                test.deepEqual(
-                  buffer,
-                  [
-                    {index: 4, key: 'b', value: 'ball'},
-                    {index: 2, key: 'a', value: 'atom'}
-                  ],
-                  'rewinds compacted log'
-                )
-                test.end()
-              })
-            })
-          })
-        })
+  runSeries([
+    log.set.bind(log, 'a', 'apple'),
+    log.set.bind(log, 'a', 'atom'),
+    log.set.bind(log, 'b', 'bicycle'),
+    log.set.bind(log, 'b', 'ball'),
+    log.set.bind(log, 'c', 'cat'),
+    log.unset.bind(log, 'c'),
+    function (done) {
+      var buffer = []
+      log.createRewindStream()
+      .on('data', function (entry) { buffer.push(entry) })
+      .once('error', function (error) { test.fail(error) })
+      .once('end', function () {
+        test.deepEqual(
+          buffer,
+          [
+            {index: 4, key: 'b', value: 'ball'},
+            {index: 2, key: 'a', value: 'atom'}
+          ],
+          'rewinds compacted log'
+        )
+        done()
       })
-    })
-  })
+    }
+  ], test.end)
 })
 
 function testInstance () {
